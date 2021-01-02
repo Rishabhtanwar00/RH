ce8c3587ef44dc408f2af0493695d7ca12884479
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2021-01-02 15:03:01 +0530

    .

--- src/pages/dashboard/dashboard.page.jsx
+++ src/pages/dashboard/dashboard.page.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import "./dashboard.styles.css";
-import SenderImage from "../../assets/home-1.png";
-import CustomButton from "../../components/custombutton/custombutton.component";
 
-export default function DashBoard() {
-  return (
-    <div className="containerEmailPage">
-      <div className="containerButtons">
-        <CustomButton guideButton className="searchBloodBtn">
-          Search Blood
-        </CustomButton>
-        <CustomButton guideButton className="updateBloodBtn">
-          Update Blood
-        </CustomButton>
-        <CustomButton guideButton className="addPostBtn">
-          Add Post
-        </CustomButton>
-        <CustomButton guideButton className="sendEmailsBtn">
-          send Emails
-        </CustomButton>
+import Navbar from "../../components/navbar/navbar.component";
+
+import DonationHistory from "../../components/donationhistory/donationhistory.component";
+
+import NavbarForMobile from "../../components/navbarformobile/navbar.component";
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="dashboard-container">
+        <Navbar />
+        <div className="dashboard-details">
+          <div className="name">Welcome Olive Max</div>
+          <DonationHistory />
+        </div>
+        <NavbarForMobile />
       </div>
-      <img src={SenderImage} alt="imageSend" className="senderImage" />
-      <p className="mailStatem-ent">Mail sends to every donor</p>
-    </div>
-  );
+    );
+  }
 }
+
+export default Dashboard;
