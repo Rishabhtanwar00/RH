d7527195a0a12112eb8da4158e9416da0eeeb613
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2021-01-02 20:43:43 +0530

    adding dashboard

--- src/components/donationhistory/donationhistory.component.jsx
+++ src/components/donationhistory/donationhistory.component.jsx
@@ -62,6 +62,83 @@ const DonationHistory = () => {
             <span>106467</span>
           </div>
         </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
+        <div className="donation-info">
+          <div className="donation-address">
+            <p className="address">Raktdaan camp,Vaishali Sector 4,gaziabhad</p>
+            <div className="date">17 November 2020 4:30pm</div>
+          </div>
+          <div className="other-info">
+            <span>12</span>
+            <span className="unit">1 unit</span>
+            <span>106467</span>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/donordetails/donordetails.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./donordetails.styles.css";
+
+class DonorDetails extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div className="donordetails-container"></div>;
+  }
+}
