315e781a561a9db152505b7ce0e3933ec00212b6
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2021-01-07 13:45:12 +0530

    changing navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,51 +1,84 @@
-import React from 'react';
+import React from "react";
 
-import {MenuItems} from "./menuitems";
+import { MenuItems } from "./menuitems";
 
-import './navbar.styles.css';
+import "./navbar.styles.css";
 
-import CustomButton from '../custombutton/custombutton.component';
+import CustomButton from "../custombutton/custombutton.component";
 
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import Registration from '../../pages/registration/registration.page';
-
-class Navbar extends React.Component{
-/*
-    state = {clicked : false}
-    handleClick = () =>{
-    this.setState({clicked : !this.state.clicked})
-    }
-*/
-    render()
-    {
-        return(
-            <nav className = "NavbarItems">
-            <img className = "navbar-logo" src = {logo} alt="navbar-logo"></img> 
-             {/* <div className = "menu-icon" onClick = {this.handleClick}>
-            <i className = {this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div> */}
-        {/* <ul className = {this.state.clicked ? 'nav-menu active' : 'nav-menu'}>  */}
-            <div className="links">
-            <div className = "nav-menu">
-                {MenuItems.map((item , index) => {
-                    return(
-                        <Link key = {index} className = {item.cName} href = {item.url} to={item.to}>
-                           {item.title}
-                        </Link>
-                    )
-                } )}
-            </div>
-            <div className="buttons">
-               <Link to='/registration'><CustomButton className='nav-buttons' navBarButton to>Register</CustomButton></Link>
-               <Link to='/login'><CustomButton className='nav-buttons' navBarButton>Log in</CustomButton></Link>
-            </div>
-            </div>
-            </nav>
-        )
-    }
-}
+const Navbar = () => {
+  return (
+    <nav className="NavbarItems">
+      <img className="navbar-logo" src={logo} alt="navbar-logo"></img>
+      <div className="links">
+        <div className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                className={item.cName}
+                href={item.url}
+                to={item.to}
+              >
+                {item.title}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="buttons">
+          <Link to="/registration">
+            <CustomButton className="nav-buttons" navBarButton>
+              Register
+            </CustomButton>
+          </Link>
+          <Link to="/login">
+            <CustomButton className="nav-buttons" navBarButton>
+              Log in
+            </CustomButton>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export const NavbarAfter = () => {
+  return (
+    <nav className="NavbarItems">
+      <img className="navbar-logo" src={logo} alt="navbar-logo"></img>
+      <div className="links">
+        <div className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                className={item.cName}
+                href={item.url}
+                to={item.to}
+              >
+                {item.title}
+              </Link>
+            );
+          })}
+        </div>
+        <div className="buttons">
+          <Link to="/dashboard">
+            <CustomButton className="nav-buttons" navBarButton>
+              dashboard
+            </CustomButton>
+          </Link>
+          <Link to="/">
+            <CustomButton className="nav-buttons" navBarButton>
+              Log out
+            </CustomButton>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
